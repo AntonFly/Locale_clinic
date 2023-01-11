ed7a97ee6440ff01cfc5266b6ac57592cc906c58
XamLua <black.hornetnikita@gmail.com>
2023-01-11 14:38:27 +0300

    implement get client(s) endpoints for engi

--- back/src/main/java/com/clinic/controllers/EngineerController.java
+++ back/src/main/java/com/clinic/controllers/EngineerController.java
@@ -1,9 +1,12 @@
 package com.clinic.controllers;
 
 import com.clinic.entities.AccompanimentScript;
+import com.clinic.entities.Client;
 import com.clinic.entities.Order;
 import com.clinic.exceptions.ConfirmationMissingException;
+import com.clinic.exceptions.NoPersonToClientException;
 import com.clinic.exceptions.OrderNotFoundException;
+import com.clinic.exceptions.PassportNotFoundException;
 import com.clinic.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,19 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController()
 //@PreAuthorize("hasRole('ROLE_ENGINEER')")
 @RequestMapping("/engineer")
 public class EngineerController {
 
+    private final ClientService clientService;
     private final FileService fileService;
     private final OrderService orderService;
 
     @Autowired
     public EngineerController(
+            ClientService cs,
             FileService fs,
             OrderService os
-    ){
+    )
+    {
+        this.clientService = cs;
         this.fileService = fs;
         this.orderService = os;
     }
@@ -39,5 +48,14 @@ public class EngineerController {
 
         return order.getAccompanimentScript();
     }
+
+    @GetMapping("/get_client_by_passport")
+    public Client getClientByPassport(@RequestParam long passport) throws
+            PassportNotFoundException, NoPersonToClientException
+    { return clientService.getClientByPassport(passport); }
+
+    @GetMapping("/get_all_clients")
+    public List<Client> getAllClients()
+    { return clientService.getAllClients(); }
 
 }
