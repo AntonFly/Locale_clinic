ebafd88e3323631122d7f69f8dde8f17bfcb7d62
AntonFly <anton99910@gmail.com>
2023-01-15 20:12:11 +0300

    back: usecase 4

--- front/SAUT/src/app/Medics/_services/support.service.ts
+++ front/SAUT/src/app/Medics/_services/support.service.ts
@@ -6,22 +6,22 @@ import { Order } from 'src/app/_models/Order';
 
 @Injectable()
 export class SupportService {
-  
+
   baseUrl:string;
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) { 
-    this.baseUrl = baseUrl;                 
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.baseUrl = baseUrl;
   }
-     
 
-  getSupportBySpecializId(spec: string){
-    let body = "?name="+spec;
-    return this.http.get<Order[]>(this.baseUrl+"medic/get_scenarios_by_spec/"+body);
+
+  getSupportBySpecializId(orderId: string) {
+    const body = '?orderId=' + orderId;
+    return this.http.get<Order[]>(this.baseUrl+"medic/get_script_by_order/"+body);
   }
 
   getClient(pass: number) {
     let body = "?passport=" + pass;
-    return this.http.get<Client>(this.baseUrl+"medic/get_client_by_passport/" + body);                
+    return this.http.get<Client>(this.baseUrl+"medic/get_client_by_passport/" + body);
   }
 
   getOrdersByPassport(id: number) {
